4a570d5702598876c43e11f0fb7294fef8e4523e
luisgabriel1 <oluisgabriel02@gmail.com>
2025-09-04 23:14:08 -0300

    portaAPI

--- backend/app.py
+++ backend/app.py
@@ -86,4 +86,4 @@ def send_email():
         return jsonify({"success": False, "error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=55000, debug=True)
